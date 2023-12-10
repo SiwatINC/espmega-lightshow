ab7f19a080034cc8fc26961ae2e3383a83bba121
Siwat Sirichai <siwat@siwatinc.com>
2023-12-10 16:41:34 +0700

    update to 3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='espmega_lightshow',
-    version='3.0',
+    version='3.1',
     license='Apache 2.0',
     author="Siwat Sirichai",
     author_email='siwat@siwatinc.com',
